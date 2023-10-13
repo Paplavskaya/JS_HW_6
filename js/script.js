991153f56f79bcd2493df948904f6d44e6dfcc2b
Paplavskaya <any-lion@mail.ru>
2023-10-13 17:01:29 +0300

    feat - added task_2 in script.js

--- js/script.js
+++ js/script.js
@@ -5,3 +5,11 @@ const replacedStr = originalStr.replace(/@/g, '!');
 console.log(replacedStr);
 
 
+// В переменной date лежит дата в формате 2025-12-31. Преобразуйте эту дату в формат 31/12/2025. 
+
+const strDate = '2025-12-31';
+const arrDate = strDate.split('-');
+const newDate = `${arrDate[2]}/${arrDate[1]}/${arrDate[0]}`;
+console.log(newDate);
+
+
